d3b02075b937f538b69976d19131cf8c8c79e8c9
Aakash Goel <goelaakash79@gmail.com>
2019-12-25 02:55:58 +0530

    server created

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const app = express();
+
+require("dotenv").config();
+require("./config/dbconnection");
+
+
+startServer = async () => {
+    try {
+        await app.listen(process.env.PORT);
+        console.log("Server is up and running");
+    }
+    catch(err) {
+        console.log("Error occurred while intialising the server.");
+    }
+}
+
+startServer();
