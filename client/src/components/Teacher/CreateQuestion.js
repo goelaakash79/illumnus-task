c4563f0aa02478fd1055450b4d787969137db9f9
Aakash Goel <goelaakash79@gmail.com>
2019-12-27 00:10:04 +0530

    create question ui added

--- client/src/components/Teacher/CreateQuestion.js
+++ client/src/components/Teacher/CreateQuestion.js
@@ -7,7 +7,61 @@ const CreateQuestion = () => {
       <div className="container mt-5">
         <h4>illumnus | Quiz Portal Task</h4>
         <hr />
-        
+        <p style={{fontSize: 20, margin:0}}>Add a new question</p>
+        <div className="section mt-4">
+          <input
+            placeholder="enter your question"
+            name="question"
+            className="form-control mb-4"
+          />
+          <div className="row">
+            <div className="col-md-3">
+              <input
+                placeholder="enter option 1"
+                name="option1"
+                className="form-control mb-4"
+              />
+            </div>
+            <div className="col-md-3">
+              <input
+                placeholder="enter option 2"
+                name="option2"
+                className="form-control mb-4"
+              />
+            </div>
+            <div className="col-md-3">
+              <input
+                placeholder="enter option 3"
+                name="option3"
+                className="form-control mb-4"
+              />
+            </div>
+            <div className="col-md-3">
+              <input
+                placeholder="enter option 4"
+                name="option4"
+                className="form-control mb-4"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <input
+                placeholder="enter correct option number"
+                name="answer"
+                className="form-control mb-4"
+              />
+            </div>
+            <div className="col-md-6">
+              <input
+                placeholder="enter question marks"
+                name="marks"
+                className="form-control mb-4"
+              />
+            </div>
+          </div>
+          <button className="btn btn-primary">Add Question</button>
+        </div>
       </div>
     </div>
   );
