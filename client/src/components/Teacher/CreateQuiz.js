32ee3363b663af42ec9c05c84c65f3e43b12df41
Aakash Goel <goelaakash79@gmail.com>
2019-12-27 00:30:10 +0530

    create quiz ui added

--- client/src/components/Teacher/CreateQuiz.js
+++ client/src/components/Teacher/CreateQuiz.js
@@ -7,7 +7,64 @@ const CreateQuiz = () => {
       <div className="container mt-5">
         <h4>illumnus | Quiz Portal Task</h4>
         <hr />
-        
+        <p style={{ fontSize: 20, margin: 0 }}>Create Quiz</p>
+        <div className="section mt-4">
+          <input
+            placeholder="enter quiz title"
+            name="title"
+            className="form-control mb-4"
+          />
+
+          <div className="row">
+            <div className="col-md-6">
+              <input
+                placeholder="enter subject"
+                name="subject"
+                className="form-control mb-4"
+              />
+            </div>
+            <div className="col-md-6">
+              <input
+                placeholder="enter duration (in minutes)"
+                name="duration"
+                className="form-control mb-4"
+              />
+            </div>
+          </div>
+
+          <label>Choose questions for the quiz</label>
+          <div className="questions-section mb-4">
+            <table className="table ">
+              <tr>
+                <th>Pick</th>
+                <th>Question</th>
+                <th>Marks</th>
+              </tr>
+              <tr>
+                <td>
+                  <input type="checkbox"></input>
+                </td>
+                <td>This is a nice question?</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="checkbox"></input>
+                </td>
+                <td>This is a nice question?</td>
+                <td>5</td>
+              </tr>
+            </table>
+            {/* <input type="checkbox"></input> <i>Question</i>- This is a beautiful
+            question? <br/>
+            <input type="checkbox"></input> <i>Question</i>- This is a beautiful
+            question? <br/>
+            <input type="checkbox"></input> <i>Question</i>- This is a beautiful
+            question? <br/> */}
+          </div>
+
+          <button className="btn btn-primary">Create Quiz</button>
+        </div>
       </div>
     </div>
   );
